701187816d620d70bc87a4d2c2a56f10629ec238
t-joe <tuugii.soft@gmail.com>
2019-12-12 22:39:49 -0600

    Add files via upload

--- scripts/maze.js
+++ scripts/maze.js
@@ -1,6 +1,31 @@
 $(document).ready(function () {
+    var started = false;
+    $("div.boundary").mouseenter(function(){
+        if(started == true)
+            lose();
+    });
 
-       $("div.boundary.boundary").mouseenter(function(){
-           $(this).addClass("boundaryRed");
-       });
+    $("div#start").click(function(){
+        $("div.boundaryRed").removeClass("boundaryRed");
+        started = true;
+        $("#status").text("Started");
+    });
+
+    $("div#end").mouseenter(function(){
+        if(started == true) {
+            $("div#end").removeClass("boundaryRed");
+            $("#status").text("You win. :)");
+            started = false;
+        }
+    });
+    $("#maze").mouseleave(function () {
+        if(started == true)
+            lose();
+    });
+
+    function lose() {
+        $("div.boundary").addClass("boundaryRed");
+        $("#status").text("Sorry, You lose. :(");
+        started = false;
+    }
 });
